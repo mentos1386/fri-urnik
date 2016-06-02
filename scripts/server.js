609f3649c7b9119f7247fab25c70651851473f64
Metod Medja <metod@medja.net>
2016-06-02 19:42:00 +0000

    feat(project): add development api proxy

--- scripts/server.js
+++ scripts/server.js
@@ -6,10 +6,18 @@ import Config from '../config/development';
 const config = new Config();
 const compiler = webpack(config.webpack);
 
+const proxy = {
+    '/api*': {
+        changeOrigin: true,
+        target: process.env.API
+    }
+};
+
 const server = new WebpackDevServer(compiler, {
+    proxy,
     hot: true,
     historyApiFallback: true,
-    contentBase: config.output.path
+    contentBase: config.output.path,
 });
 
 server.listen(process.env.PORT);
