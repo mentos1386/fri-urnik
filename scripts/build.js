ccf8febc77355fdb50f9ce38b55f3a7a1c475b3b
Metod Medja <metod@medja.net>
2016-06-22 15:12:55 +0000

    feat(project): add analysis command

--- scripts/build.js
+++ scripts/build.js
@@ -7,9 +7,13 @@ process.env.NODE_ENV = 'production';
 const config = new Config();
 const compiler = webpack(config.webpack);
 
+const json = process.argv.slice(-1)[0] === 'json';
+
 compiler.run((error, stats) => {
     if (error) {
         console.log(`Error: ${error}`);
+    } else if (json) {
+        console.log(JSON.stringify(stats.toJson()));
     } else {
         const elapsed = (stats.endTime - stats.startTime) / 1e3;
         console.log(`Finished build in ${elapsed.toFixed(2)} seconds`);
